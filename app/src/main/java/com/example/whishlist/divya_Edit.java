677f015c846c201958d317b641a6af95527fe541
NETHMI <nethmitharushika1999@gmail.com>
2020-08-17 02:04:28 +0530

    Divya_initial_comment

---
+++ app/src/main/java/com/example/whishlist/divya_Edit.java
@@ -0,0 +1,14 @@
+package com.example.whishlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class divya_Edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.divya_activity_edit);
+    }
+}
